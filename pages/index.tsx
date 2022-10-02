ae8ad9cab66d3ec113915b7c2784befa353c4b81
Davi <al.davigvalerio@gmail.com>
2022-10-02 18:52:19 -0300

    refactor(home): align text

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,19 @@
 import type { NextPage } from 'next';
 
-import { Typography } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 
 const Home: NextPage = () => {
   return (
-    <div>
-      <main>
-        <Typography variant="h1">Quanto Gastei?</Typography>
-        <Typography variant="h2">Home Page</Typography>
-      </main>
-    </div>
+    <Grid container justifyContent="center">
+      <Grid item>
+        <Typography variant="h1" align="center">
+          Quanto Gastei?
+        </Typography>
+        <Typography variant="h2" align="center">
+          Home Page
+        </Typography>
+      </Grid>
+    </Grid>
   );
 };
 
