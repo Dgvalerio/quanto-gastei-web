306fbcbb12a47e00a0d08bd280931ff50f9af863
Davi <al.davigvalerio@gmail.com>
2022-10-08 18:10:10 -0300

    refactor(operation-type): improve responsive design

--- src/operation-type/components/read-all/operation-type.read-all.tsx
+++ src/operation-type/components/read-all/operation-type.read-all.tsx
@@ -9,7 +9,7 @@ import OperationTypeTypes from '@/src/operation-type/operation-type.types';
 const LoadingSkeleton: FC = () => (
   <>
     {[...new Array(6)].map((_, i) => (
-      <Grid key={i} item xs={4}>
+      <Grid key={i} item xs={12} sm={6} md={4}>
         <Skeleton width="100%" height={128} sx={{ transform: 'none' }} />
       </Grid>
     ))}
@@ -30,7 +30,7 @@ const OperationTypeReadAll: OperationTypeTypes.ReadAllComponent = () => {
           <LoadingSkeleton />
         ) : (
           operationTypes.map((type) => (
-            <Grid item xs={4} key={type.id}>
+            <Grid item xs={12} sm={6} md={4} key={type.id}>
               <OperationTypeItem {...type} />
             </Grid>
           ))
