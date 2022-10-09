e6b2827b149011c55a9371d9fbab535837afc95e
Davi <al.davigvalerio@gmail.com>
2022-10-08 23:30:46 -0300

    refactor(theme): compare theme mode correctly

--- styles/theme.ts
+++ styles/theme.ts
@@ -6,6 +6,6 @@ const darkTheme: ThemeOptions = { palette: { mode: 'dark' } };
 const lightTheme: ThemeOptions = { palette: { mode: 'light' } };
 
 const getTheme = (themeMode: 'light' | 'dark'): Theme =>
-  createTheme(themeMode === 'dark' ? lightTheme : darkTheme);
+  createTheme(themeMode === 'light' ? lightTheme : darkTheme);
 
 export default getTheme;
