c43b5a67ec30893c0d6f245c04cfd69d02bd4649
Davi <al.davigvalerio@gmail.com>
2022-10-08 23:18:38 -0300

    refactor(operation-type): add return to no items

--- src/operation-type/components/read-all/operation-type.read-all.tsx
+++ src/operation-type/components/read-all/operation-type.read-all.tsx
@@ -1,19 +1,21 @@
 import { FC, useEffect } from 'react';
 
-import { Box, Grid, Paper, Skeleton } from '@mui/material';
+import { Box, Grid, Paper, Skeleton, Typography } from '@mui/material';
 
 import OperationTypeItem from '@/src/operation-type/components/read-one/operation-type.item';
 import useOperationTypeStore from '@/src/operation-type/operation-type.store';
 import OperationTypeTypes from '@/src/operation-type/operation-type.types';
 
 const LoadingSkeleton: FC = () => (
-  <>
-    {[...new Array(6)].map((_, i) => (
-      <Grid key={i} item xs={12} sm={6} md={4}>
-        <Skeleton width="100%" height={128} sx={{ transform: 'none' }} />
-      </Grid>
-    ))}
-  </>
+  <Box component={Paper} p={2}>
+    <Grid container spacing={2}>
+      {[...new Array(6)].map((_, i) => (
+        <Grid key={i} item xs={12} sm={6} md={4}>
+          <Skeleton width="100%" height={128} sx={{ transform: 'none' }} />
+        </Grid>
+      ))}
+    </Grid>
+  </Box>
 );
 
 const OperationTypeReadAll: OperationTypeTypes.ReadAllComponent = () => {
@@ -23,11 +25,15 @@ const OperationTypeReadAll: OperationTypeTypes.ReadAllComponent = () => {
     void loadAll();
   }, [loadAll]);
 
+  if (loading) return <LoadingSkeleton />;
+
   return (
     <Box component={Paper} p={2}>
       <Grid container spacing={2}>
-        {loading ? (
-          <LoadingSkeleton />
+        {operationTypes.length === 0 ? (
+          <Grid item xs={12}>
+            <Typography>Não há items para serem exibidos.</Typography>
+          </Grid>
         ) : (
           operationTypes.map((type) => (
             <Grid item xs={12} sm={6} md={4} key={type.id}>
